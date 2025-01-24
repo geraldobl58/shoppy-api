4b0d61ec83bd7c2677d4938f318e0d7f01fdd7ba
Geraldo Luiz <geraldobl58@gmail.com>
2025-01-24 08:51:05 -0300

    feat: add Prisma module and service for database integration, update UsersService to use Prisma for user creation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUserRequest) {
-    console.log(data);
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: CreateUserRequest) {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists');
+      }
+      throw error;
+    }
   }
 }
